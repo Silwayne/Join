9e2f4e6f83823e781a8f7b4541ae10f3a83902e4
Christian Böhme <121933301+WorldChris380@users.noreply.github.com>
2025-02-19 16:49:20 +0100

    js for user initials contacts page

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,1 +1,11 @@
 let leftContactsColumn = document.getElementById("left-contacts-page-column");
+let userName = document.getElementById("user-name-index").innerText;
+
+function createContactNameInitials() {
+  let firstLetterOfUserName = userName.charAt(0);
+  let secondLetterOfUserName = userName.split(" ")[1][0];
+  let userImage = document.getElementById("user-icon-index");
+  userImage.innerHTML = `${firstLetterOfUserName}${secondLetterOfUserName}`;
+  userImage.classList.add("user-initials");
+}
+createContactNameInitials();
