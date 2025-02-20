c75eff829a90626707c72dcbb7eea19468524e6d
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2025-02-20 20:17:45 +0100

    Login Screen with Animation

--- register/log-in/script/log-in.js
+++ register/log-in/script/log-in.js
@@ -1,21 +1,23 @@
 const firebaseURL = "https://join-log-in-1761a-default-rtdb.europe-west1.firebasedatabase.app/users.json";
 
-// function login() {
-//     fetch(firebaseURL)
-//         .then(response => response.json())
-//         .then(users => {
-//             let email = document.getElementById("email").value;
-//             let password = document.getElementById("password").value;
-
-//             let userList = Object.values(users || {}); 
-//             let userFound = userList.find(user => user.email === email && user.password === password);
-
-//             userFound 
-//                 ? (alert("Login erfolgreich!"), window.location.href = "./join-main/join-main.html") 
-//                 : alert("Falsche E-Mail oder Passwort!");
-//         })
-//         .catch(console.error);
-// }
+window.onload = function () {
+    const loader = document.getElementById('loader');
+    const logo = document.getElementById('logo');
+    const content = document.getElementById('content');
+  
+    setTimeout(() => {
+      logo.style.width = '100px'; 
+      logo.style.height = '122px';
+      logo.style.top = '80px'; 
+      logo.style.left = '70px';
+      logo.style.transform = 'translate(0, 0)'; 
+      loader.style.opacity = '0';
+      
+      setTimeout(() => {
+        loader.style.display = 'none';
+      }, 1000); 
+    }, 500); 
+  };
 
 function login() {
     let email = document.getElementById("email").value;
