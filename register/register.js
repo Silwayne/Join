347d774ed69024bb44a0e229c25fc5b8920557e5
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2025-02-20 10:46:17 +0100

    Username and Confirm Password added

--- register/register.js
+++ register/register.js
@@ -2,10 +2,18 @@
 const firebaseURL = "https://join-log-in-1761a-default-rtdb.europe-west1.firebasedatabase.app/users.json";
 
 function registerUser() {
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirm-password").value;
+
+    if (password !== confirmPassword) {
+        alert("Passwörter stimmen nicht überein!");
+        return;
+    }
     fetch(firebaseURL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
+            name: document.getElementById("first-name").value,
             email: document.getElementById("email").value,
             password: document.getElementById("password").value
         })
