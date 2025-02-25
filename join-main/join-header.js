b63e1f94466a21fc63945950dd0263df4352d3ca
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2025-02-25 11:14:11 +0100

    Help Page

--- join-main/join-header.js
+++ join-main/join-header.js
@@ -2,7 +2,7 @@ function renderHeader(){
     document.getElementById('header').innerHTML = `
         <span class="headline">Kanban Project Management Tool</span>
     <div class="header-right">
-      <img class="help-button" src="/assets/img/perm_contact_calendar.svg" alt="Help">
+      <img class="help-button" src="/assets/img/help.svg" alt="Help">
       <span class="user">G</span>
     </div>
     `
