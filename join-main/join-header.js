88ca2c19815ee3828c04a6adc4ce785ddbd129e9
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2025-02-26 19:49:18 +0100

    Legal, Help and Privacy. Change CSS Style

--- join-main/join-header.js
+++ join-main/join-header.js
@@ -2,7 +2,7 @@ function renderHeader(){
     document.getElementById('header').innerHTML = `
         <span class="headline">Kanban Project Management Tool</span>
     <div class="header-right">
-      <img class="help-button" src="/assets/img/help.svg" alt="Help">
+      <a href="help.html"><img class="help-button" src="/assets/img/help.svg" alt="Help"></a>
       <span class="user">G</span>
     </div>
     `
--- join-main/side-bar.js
+++ join-main/side-bar.js
@@ -1,39 +1,43 @@
 function renderSidebar() {
   document.getElementById("sidbar").innerHTML = `
 
-     <nav class="sidbar-nav">
-          <div class="sidbar-top">
+    <nav class="sidbar-nav">
+        <div class="sidbar-top">
               <img class="sidbar-logo" src="/assets/img/Capa 2.svg" alt="Logo">
-              <div class="menu">
-                  <button class="menu-button menu-button-design font-size">
+                <div class="menu">
+                    <button class="menu-button menu-button-design font-size">
                       <img src="/assets/img/summary-icon.svg" alt="Summary">
                       <span>Summary</span>
-                  </button>
-                  <button class="menu-button menu-button-design font-size">
+                    </button>
+                    <button class="menu-button menu-button-design font-size">
                       <img src="/assets/img/add_task_icon.svg" alt="Add Task">
                       <span>Add Task</span>
-                  </button>
-                  <button class="menu-button menu-button-design font-size">
+                    </button>
+                    <button class="menu-button menu-button-design font-size">
                       <img src="/assets/img/board-icon.svg" alt="Board">
                       <span>Board</span>
-                  </button>
-                  <a href="contacts.html">
-                    <button href="contacts.html" class="menu-button menu-button-design font-size">
-                        <img src="/assets/img/perm_contact_calendar.svg" alt="Contacts">
-                        <span>Contacts</span>
+                    </button>
+                    <a href="contacts.html">
+                        <button href="contacts.html" class="menu-button menu-button-design font-size">
+                            <img src="/assets/img/perm_contact_calendar.svg" alt="Contacts">
+                            <span>Contacts</span>
                       </button>
-                  </a>
-              </div>
+                    </a>
+                </div>
           </div>
           <div class="sidbar-bottom">
-              <button class="menu-button menu-button-lower-design font-size">
-                  Privacy Policy
-              </button>
-              <button class="menu-button menu-button-lower-design font-size">
-                  Legal Notice
-              </button>
-          </div>
-      </nav>
+            <a href="privacy.html">
+                <button class="menu-button menu-button-lower-design font-size">
+                    Privacy Policy
+                </button>
+            </a>
+            <a href="legal.html">
+                <button class="menu-button menu-button-lower-design font-size">
+                    Legal Notice
+                </button>
+            </a>
+        </div>
+    </nav>
 
 
 `;
