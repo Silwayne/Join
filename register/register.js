932a64037ec1d1f5c938e0688cc707b0fd156a84
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2025-03-27 09:13:54 +0100

    Color for Users

--- register/register.js
+++ register/register.js
@@ -15,12 +15,10 @@ function showMessage(message, isSuccess = false) {
 }
 
 function getRandomColor() {
-  let letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+  let colors = ["#FF7A00", "#FF5EB3", "#6E52FF", "#9327FF", "#00BEE8", "#1FD7C1", "#FF745E", "#FFA35E", "#FC71FF", "#FFC701", "#0038FF", "#C3FF2B", "#FFE62B", "#FF4646", "#FFBB2B"];
+
+  let randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
 }
 
 function registerUser() {
