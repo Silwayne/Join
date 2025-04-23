8b5b397c21777c37c3f58f35c65035b1d8819a26
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2025-04-23 15:43:39 +0200

    Tasks on Join Main added

--- join-main/join-main.js
+++ join-main/join-main.js
@@ -1,10 +1,39 @@
+const firebaseURL = "https://join-log-in-1761a-default-rtdb.europe-west1.firebasedatabase.app/";
+
 function init(){
     renderHeader();
     initDropdownEvents();
     renderSidebar();
     renderSidebarResponsive();
     updateDate();
+    updateTaskCounters();
   }
+
+  async function updateTaskCounters() {
+    const response = await fetch(firebaseURL + "tasks.json");
+    const data = await response.json();
+  
+    const counts = {todo: 0, done: 0, urgent: 0, total: 0, progress: 0, feedback: 0};
+  
+    for (let key in data) {
+      const task = data[key];
+  
+      counts.total++;
+  
+      if (task.status === "todo") counts.todo++;
+      if (task.status === "done") counts.done++;
+      if (task.status === "inprogress") counts.progress++;
+      if (task.status === "await") counts.feedback++;
+      if (task.priority?.toLowerCase() === "urgent") counts.urgent++;
+    }
+  
+    document.getElementById("count-todo").textContent = counts.todo;
+    document.getElementById("count-done").textContent = counts.done;
+    document.getElementById("count-urgent").textContent = counts.urgent;
+    document.getElementById("count-total").textContent = counts.total;
+    document.getElementById("count-progress").textContent = counts.progress;
+    document.getElementById("count-feedback").textContent = counts.feedback;
+  }  
 
   function getGreetingText() {
     let hours = new Date().getHours();
