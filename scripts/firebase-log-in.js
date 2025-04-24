bf9aa97bfbee337487a61bf25ad5ff3c9267bbfc
Ronny Pollmer <168807956+Silwayne@users.noreply.github.com>
2025-04-24 20:46:53 +0200

    Firebase Test 3

--- scripts/firebase-log-in.js
+++ scripts/firebase-log-in.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-const firebaseURLUser = "https://join-log-in-1761a-default-rtdb.europe-west1.firebasedatabase.app/users.json";
-
-
-
 const firebaseURL = "https://join-log-in-1761a-default-rtdb.europe-west1.firebasedatabase.app/";
-=======
+
 const firebaseURLUser =
   "https://join-log-in-1761a-default-rtdb.europe-west1.firebasedatabase.app/users.json";
 
@@ -16,4 +11,3 @@ const firebaseForDeletion =
 
 const firbaseForTasks =
   "https://join-log-in-1761a-default-rtdb.europe-west1.firebasedatabase.app/tasks/";
->>>>>>> e88b73e221f37efd0b0607fea6539fdf52b5f972
